6372f507a32e434b5c928c1f300283125c12e11d
root <root@debian.localdomain>
2009-07-09 22:57:56 -0400

    attempt to push forward ACPI initialization: crude implementation of semaphores

---
+++ kernel/semaphore.h
@@ -0,0 +1,54 @@
+#ifndef _SEMAPHORE_H_
+#define _SEMAPHORE_H_
+#include "kernel.h"
+#include "spinlock.h"
+
+struct semaphore {
+  int s, max;
+  struct spinlock lock;
+  unsigned short waitqueue;
+};
+
+static inline int semaphore_init(struct semaphore *sem, int max, int init) {
+  sem->s = init;
+  sem->max = max;
+  sem->waitqueue = 0;
+  spinlock_init(&sem->lock);
+  return 0;
+}
+
+static inline int semaphore_signal(struct semaphore *sem, int s) {
+  int status = 0;
+  spinlock_lock(&sem->lock);
+  sem->s += s;
+  if (sem->s > sem->max) {
+    sem->s = sem->max;
+    status = -1;
+  }
+  /* wake up waiters */
+  wakeup_list(sem->waitqueue);
+  spinlock_unlock(&sem->lock);
+  return status;
+}
+
+/* timeout: millisec, (-1) for indefinite */
+static inline int semaphore_wait(struct semaphore *sem, int s, short timeout) {
+  for(;;) {
+    spinlock_lock(&sem->lock);
+    if (sem->s >= s) {
+      sem->s -= s;
+      spinlock_unlock(&sem->lock);
+      return 0;
+    } else {
+      spinlock_unlock(&sem->lock);
+      schedule();
+    }
+  }
+}
+
+static inline int semaphore_destroy(struct semaphore *sem) {
+  return 0;
+}
+
+
+#endif
