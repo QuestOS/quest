f45f128537e7c5359748fb3d6576afecb64d763c
Matthew Danish <md@bu.edu>
2009-07-25 17:43:54 -0400

    move memset/cpy into i386.h

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -109,27 +109,6 @@ void stacktrace (void);
 
 void tsc_delay_usec (uint32 usec);
 
-static inline void *
-memset (void *p, int ch, uint32 cb)
-{
-
-  asm volatile ("cld; rep stosb"
-                :"=D" (p), "=a" (ch), "=c" (cb)
-                :"0" (p), "1" (ch), "2" (cb)
-                :"memory","flags");
-  return p;
-}
-
-static inline void *
-memcpy (void *pDest, const void *pSrc, uint32 cb)
-{
-  asm volatile ("cld; rep movsb"
-                :"=c" (cb), "=D" (pDest), "=S" (pSrc)
-                :"0" (cb), "1" (pDest), "2" (pSrc)
-                :"memory","flags");
-  return pDest;
-}
-
 /* Declare space for a stack */
 extern uint32 ul_stack[][1024] __attribute__ ((aligned (4096)));
 
