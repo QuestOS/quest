a9bc8e3116040aa89d38496a5a3a6dd7ed3451d5
Matthew <md@bu.edu>
2010-06-06 14:21:39 -0400

    Insert GPL license header in every relevant file.

--- kernel/drivers/net/ethernetif.c
+++ kernel/drivers/net/ethernetif.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001-2004 Swedish Institute of Computer Science.
+ * Portions Copyright (c) 2001-2004 Swedish Institute of Computer Science.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -30,10 +30,24 @@
  *
  */
 
-/* Ported to Quest by: Matthew Danish (md)
+/*                    The Quest Operating System
+ *  Portions Copyright (C) 2005-2010  Richard West, Boston University
  *
- * Changes Copyright (c) 2010 Boston University
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/* Ported to Quest by: Matthew Danish (md) */
 
 #include "lwip/init.h"
 #include "lwip/opt.h"
--- kernel/include/arch/cc.h
+++ kernel/include/arch/cc.h
@@ -1,5 +1,22 @@
+/*                    The Quest Operating System
+ *  Copyright (C) 2005-2010  Richard West, Boston University
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /*
- * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ * Portions Copyright (c) 2001-2003 Swedish Institute of Computer Science.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -29,10 +46,28 @@
  * Author: Adam Dunkels <adam@sics.se>
  *
  */
+
+/*                    The Quest Operating System
+ *  Portions Copyright (C) 2005-2010  Richard West, Boston University
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Ported to Quest by: Matthew Danish (md) */
+
 #ifndef __ARCH_CC_H__
 #define __ARCH_CC_H__
-
-/* Modified by: Matthew Danish (md) while porting to Quest */
 
 #include "types.h"
 
