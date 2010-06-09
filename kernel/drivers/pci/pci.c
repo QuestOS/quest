19e922feb4a939e37844e75de7992489b44d63b9
Matthew <md@bu.edu>
2010-06-09 18:37:11 -0400

    Mask first 7 bits of headerType, last bit just indicates multi-function.

--- kernel/drivers/pci/pci.c
+++ kernel/drivers/pci/pci.c
@@ -97,29 +97,7 @@ probe (void)
     for (slot=0; slot <= PCI_MAX_DEV_NUM; slot++)
       for (func=0; func <= PCI_MAX_FUNC_NUM; func++)
         if (READ (bus, slot, func, 0x00, dword) != 0xFFFFFFFF) {
-#ifdef DEBUG_PCI
-          DLOG ("%.02x:%.02x.%x", bus, slot, func);
-          DLOG ("  %.08x %.08x %.08x %.08x",
-                READ (bus, slot, func, 0x00, dword),
-                READ (bus, slot, func, 0x04, dword),
-                READ (bus, slot, func, 0x08, dword),
-                READ (bus, slot, func, 0x0C, dword));
-          DLOG ("  %.08x %.08x %.08x %.08x",
-                READ (bus, slot, func, 0x10, dword),
-                READ (bus, slot, func, 0x14, dword),
-                READ (bus, slot, func, 0x18, dword),
-                READ (bus, slot, func, 0x1C, dword));
-          DLOG ("  %.08x %.08x %.08x %.08x",
-                READ (bus, slot, func, 0x20, dword),
-                READ (bus, slot, func, 0x24, dword),
-                READ (bus, slot, func, 0x28, dword),
-                READ (bus, slot, func, 0x2C, dword));
-          DLOG ("  %.08x %.08x %.08x %.08x",
-                READ (bus, slot, func, 0x30, dword),
-                READ (bus, slot, func, 0x34, dword),
-                READ (bus, slot, func, 0x38, dword),
-                READ (bus, slot, func, 0x3C, dword));
-#endif
+
           uint16 vendorID = READ (bus, slot, func, 0x00, word);
           uint16 deviceID = READ (bus, slot, func, 0x02, word);
           uint8 classID   = READ (bus, slot, func, 0x0B, byte);
@@ -127,9 +105,41 @@ probe (void)
           uint8 prgIFID   = READ (bus, slot, func, 0x09, byte);
           uint8 header    = READ (bus, slot, func, 0x0E, byte);
 
+          for (i=0; i<0x10; i++)
+            devices[num_devices].data[i] =
+              /* BTW, misaligned read from PCI config space causes
+               * VMware to crash. :) */
+              READ (bus, slot, func, i<<2, dword);
+
+#ifdef DEBUG_PCI
+          DLOG ("%.02x:%.02x.%x", bus, slot, func);
+          DLOG ("  %.08x %.08x %.08x %.08x",
+                devices[num_devices].data[0],
+                devices[num_devices].data[1],
+                devices[num_devices].data[2],
+                devices[num_devices].data[3]);
+          DLOG ("  %.08x %.08x %.08x %.08x",
+                devices[num_devices].data[4],
+                devices[num_devices].data[5],
+                devices[num_devices].data[6],
+                devices[num_devices].data[7]);
+          DLOG ("  %.08x %.08x %.08x %.08x",
+                devices[num_devices].data[8],
+                devices[num_devices].data[9],
+                devices[num_devices].data[10],
+                devices[num_devices].data[11]);
+          DLOG ("  %.08x %.08x %.08x %.08x",
+                devices[num_devices].data[12],
+                devices[num_devices].data[13],
+                devices[num_devices].data[14],
+                devices[num_devices].data[15]);
+#endif
+
 #ifdef DEBUG_PCI
           if (classID == 0x02 && subclID == 0)
             printf ("PCI ethernet: %x %x\n", vendorID, deviceID);
+          if (classID == 0x0C && subclID == 0x03)
+            printf ("USB host controller: %x %x\n", vendorID, deviceID);
           if (pci_search_ven_table (vendorID, &ven))
             DLOG ("  %s (0x%x)", ven.VenFull, vendorID);
           else
@@ -139,7 +149,10 @@ probe (void)
           else
             DLOG ("  0x%x", deviceID);
           if (pci_search_class_code_table (classID, subclID, prgIFID, &cc))
-            DLOG ("  %s %s %s", cc.BaseDesc, cc.SubDesc, cc.ProgDesc);
+            DLOG ("  %s (%x) %s (%x) %s (%x)",
+                  cc.BaseDesc, classID,
+                  cc.SubDesc, subclID,
+                  cc.ProgDesc, prgIFID);
 #endif
 
           devices[num_devices].vendor = vendorID;
@@ -152,13 +165,7 @@ probe (void)
           devices[num_devices].progIF = prgIFID;
           devices[num_devices].headerType = header;
 
-          for (i=0; i<0x10; i++)
-            devices[num_devices].data[i] =
-              /* BTW, misaligned read from PCI config space causes
-               * VMware to crash. :) */
-              READ (bus, slot, func, i<<2, dword);
-
-          if (header == 0) {
+          if ((header & 0x7F) == 0) {
             /* 6 BARs */
             for (i=0; i<6; i++) {
               devices[num_devices].bar[i].raw = devices[num_devices].data[4+i];
@@ -239,12 +246,13 @@ pci_decode_bar (uint index, uint bar_index,
 
   dev = &devices[index];
 
-  if (dev->headerType == 0) {
+  if ((dev->headerType & 0x7F) == 0) {
     /* there are 6 BARs */
     if (bar_index >= 6) return FALSE;
 
     bar = &dev->bar[bar_index];
 
+    DLOG ("pci_decode_bar (%d, %d) bar->raw=%p", index, bar_index, bar->raw);
     if (bar->raw & 0x1) {
       /* IO port */
       if (io_addr)
