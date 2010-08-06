ef59c30b1c2e8da61c9b520ffbbb8c0b82924493
Matthew <md@bu.edu>
2010-08-06 15:10:27 -0400

    move config options into separate file

---
+++ kernel/default-config.mk
@@ -0,0 +1,16 @@
+# Kernel configuration
+
+# Compiler optimization level
+OPT = 0
+
+# Disable SMP
+# CFG += -DNO_SMP
+
+# Disable ACPI support
+# CFG += -DNO_ACPI
+
+# Disable Intel Multiprocessor Specification parsing
+# CFG += -DNO_INTEL_MPS
+
+# Use per-CPU SMP scheduling queues
+# CFG += -DMPQ
