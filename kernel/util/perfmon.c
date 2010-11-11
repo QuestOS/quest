ca72bc2fac99b672e5579740bbde556ef6b1a1b1
Ye Li <liye@cs.bu.edu>
2010-11-11 00:05:53 -0500

    perfmon get cache info

--- kernel/util/perfmon.c
+++ kernel/util/perfmon.c
@@ -20,6 +20,7 @@
 #include "util/cpuid.h"
 #include "util/debug.h"
 #include "util/perfmon.h"
+#include "arch/i386-div64.h"
 
 #define DEBUG_PERFMON
 
@@ -38,6 +39,8 @@ static u8 bit_width, perfmon_version, num_pmcs=0;
 #define IA32_PERF_GLOBAL_OVF_CTRL 0x390
 
 #define IA32_FIXED_CTR(x) (0x309 + (x))
+
+#define IA32_LEVEL_CACHES  0x5
 
 static struct predefined_arch_perfevts {
   char *name;
@@ -62,6 +65,22 @@ static struct predefined_arch_perfevts {
 #define NUM_PREDEFINED_ARCH_PERFEVTS \
   (sizeof (predefined_arch_perfevts) / sizeof (struct predefined_arch_perfevts))
 
+static struct cache_info {
+  uint64 cache_size;             /* Total cache size in bytes */
+  unsigned char num_apic;        /* # of APIC IDs reserved for this package */
+  unsigned num_thread;           /* # of threads sharing this cache */
+  bool fully_assoc;              /* Fully associative? */
+  unsigned char self_init_level; /* Self initialising cache level */
+  unsigned char cache_level;     /* Cache level */
+  unsigned char cache_type;      /* 0 - Null, 1 - Data, 2 - Instruction, 3 - Unified */
+  unsigned associativity;        /* Ways of associativity */
+  unsigned line_part;            /* Physical line partition */
+  unsigned line_size;            /* System coherency line size */
+  unsigned sets;                 /* # of sets */
+  bool inclusive;                /* Cache inclusive to lower cache level? */
+  unsigned char invd;
+} cache_info [IA32_LEVEL_CACHES];
+
 bool perfmon_enabled = FALSE;
 bool nehalem_perfmon_enabled = FALSE;
 bool westmere_perfmon_enabled = FALSE;
@@ -79,6 +98,8 @@ offcore_perfmon_pmc_config (int x, int rsp, uint64 offcore_evts)
       if (nehalem_perfmon_enabled || westmere_perfmon_enabled) {
         wrmsr (MSR_OFFCORE_RSP (0), offcore_evts);
         perfmon_pmc_config (x, OFFCORE_RSP0_EVT, OFFCORE_RSP_MASK);
+      } else {
+        DLOG ("Off-Core Response Event is not supported");
       }
       break;
 
@@ -86,11 +107,66 @@ offcore_perfmon_pmc_config (int x, int rsp, uint64 offcore_evts)
       if (westmere_perfmon_enabled) {
         wrmsr (MSR_OFFCORE_RSP (1), offcore_evts);
         perfmon_pmc_config (x, OFFCORE_RSP1_EVT, OFFCORE_RSP_MASK);
+      } else {
+        DLOG ("MSR_OFFCORE_RSP1 is only available on Westmere");
       }
       break;
 
     default :
-      DLOG ("Off-Core Response Event is not supported");
+      DLOG ("At most 2 off-core response msr's are supported");
+  }
+}
+
+static
+void perfmon_get_cache_info (void)
+{
+  u32 eax = 0, ebx = 0, ecx = 0, edx = 0;
+  int level = 0 ,i = 0;
+
+  for (i = 0; i < IA32_LEVEL_CACHES; i++) {
+    cpuid (0x4, i, &eax, &ebx, &ecx, &edx);
+    //DLOG ("eax=0x%X, ebx=0x%x, ecx=0x%x, edx=0x%x", eax, ebx, ecx, edx);
+    if (!(eax & 0x1F)) break;
+    level = (eax >> 5) & 0x7;
+    cache_info [i].num_apic = ((eax >> 26) & 0x3F) + 1;
+    cache_info [i].num_thread = ((eax >> 14) & 0xFFF) + 1;
+    cache_info [i].fully_assoc = (eax >> 9) & 0x1;
+    cache_info [i].self_init_level = (eax >> 8) & 0x1;
+    cache_info [i].cache_level = level;
+    cache_info [i].cache_type = eax & 0x1F;
+    cache_info [i].associativity = ((ebx >> 22) & 0x3FF) + 1;
+    cache_info [i].line_part = ((ebx >> 12) & 0x3FF) + 1;
+    cache_info [i].line_size = (ebx & 0xFFF) + 1;
+    cache_info [i].sets = ecx + 1;
+    cache_info [i].inclusive = (edx >> 1) & 0x1;
+    cache_info [i].invd = edx & 0x1;
+    cache_info [i].cache_size =
+      cache_info [i].associativity * cache_info [i].line_part *
+      cache_info [i].line_size * cache_info [i].sets;
+
+    DLOG ("Level %d cache detected:", level);
+    DLOG ("  Total cache size: %lld KB", cache_info [i].cache_size / 1024);
+    switch (cache_info [i].cache_type) {
+      case 1 :
+        DLOG ("  Level %d  Data Cache", cache_info [i].cache_level);
+        break;
+      case 2 :
+        DLOG ("  Level %d  Instruction Cache", cache_info [i].cache_level);
+        break;
+      case 3 :
+        DLOG ("  Level %d  Unified Cache", cache_info [i].cache_level);
+        break;
+      default:
+        DLOG ("  Level %d  Unknown Cache", cache_info [i].cache_level);
+    }
+    if (cache_info [i].fully_assoc)
+      DLOG ("  Fully associative");
+    else
+      DLOG ("  %d way associative", cache_info [i].associativity);
+    DLOG ("  Line size: %d", cache_info [i].line_size);
+    DLOG ("  Physical line partitions: %d", cache_info [i].line_part);
+    DLOG ("  Number of sets: %d", cache_info [i].sets);
+    DLOG ("  Inclusive: %s", cache_info [i].inclusive ? "Yes" : "No");
   }
 }
 
@@ -113,6 +189,8 @@ perfmon_init (void)
       ((display & 0xFF) == 0x1A)) {
     DLOG ("Nehalem Enhancements of Performance Monitoring enabled.");
     nehalem_perfmon_enabled = TRUE;
+    /* Get cache information */
+    perfmon_get_cache_info ();
   }
 
   perfmon_version = (u8) eax;
@@ -153,6 +231,16 @@ perfmon_init (void)
   }
   RDTSC (tsc);
   DLOG ("pmc0=0x%llX tsc=0x%llX", perfmon_pmc_read (0), tsc);
+
+  /* Testing Offcore Rsp Perfmon */
+  uint64 local_miss = 0, global_miss = 0;
+
+  offcore_perfmon_pmc_config (0, 0, (uint64) 0x0 | OFFCORE_LOCAL_DRAM);
+  local_miss = perfmon_pmc_read (0);
+  DLOG ("Local L3 miss: 0x%llX", local_miss);
+  perfmon_pmc_config (0, 0x2E, 0x41);
+  global_miss = perfmon_pmc_read (0);
+  DLOG ("Global L3 miss: 0x%llX", global_miss);
 
   perfmon_enabled = TRUE;
 }
