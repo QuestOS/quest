8a1a7b6f3050295193b1ac6cca9de7ea4de9b962
Ye Li <liye@cs.bu.edu>
2011-07-16 22:01:36 -0400

    Shared memory source files added.

---
+++ kernel/include/vm/shm.h
@@ -0,0 +1,28 @@
+/*                    The Quest Operating System
+ *  Copyright (C) 2005-2010  Richard West, Boston University
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SHM_H_
+#define _SHM_H_
+
+#include <types.h>
+
+#define PHYS_SHARED_MEM_HIGH    0xC0000000
+
+extern void shm_init (void);
+
+#endif
+
---
+++ kernel/vm/shm.c
@@ -0,0 +1,42 @@
+/*                    The Quest Operating System
+ *  Copyright (C) 2005-2010  Richard West, Boston University
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "vm/vmx.h"
+#include "vm/shm.h"
+#include "vm/ept.h"
+#include "kernel.h"
+#include "mem/physical.h"
+#include "mem/virtual.h"
+#include "util/cpuid.h"
+#include "util/printf.h"
+#include "smp/apic.h"
+#include "arch/i386.h"
+#include "sched/sched.h"
+
+#if DEBUG_SHM > 0
+#define DLOG(fmt,...) DLOG_PREFIX("ept",fmt,##__VA_ARGS__)
+#else
+#define DLOG(fmt,...) ;
+#endif
+
+void
+shm_init ()
+{
+
+}
+
+/* vi: set et sw=2 sts=2: */
