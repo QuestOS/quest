80a73b044c5ccebd02e60b522b7b566e16759e9b
Ye Li <liye@cs.bu.edu>
2011-10-22 19:26:27 -0400

    Message Passing Test.

--- kernel/sched/msgt.c
+++ kernel/sched/msgt.c
@@ -24,7 +24,7 @@
 #include "mem/physical.h"
 #include "mem/virtual.h"
 
-#define DEBUG_MSGT
+//#define DEBUG_MSGT
 
 #ifdef DEBUG_MSGT
 #define DLOG(fmt,...) DLOG_PREFIX("Msg Test",fmt,##__VA_ARGS__)
@@ -33,14 +33,19 @@
 #endif
 
 #define MSG_HOST    1
-#define MB_SIZE     64
+#define MB_SIZE     64 /* In bytes */
+#define SND_RATE    50000 /* micro-seconds */
 
 static u32 msgt_stack[1024] ALIGNED (0x1000);
+static u32 ipc_stack[1024] ALIGNED (0x1000);
 static task_id msgt_id = 0;
 static u32 phys_channels[4];
 static void * virt_channels[4];
 static void * channel = NULL;
+static int recv_rate[4] = {100000, 1, 800000, 1000000};
 u8 * mailbox;
+
+static uint64 logs[4][50];
 
 static void
 msg_thread (void)
@@ -48,6 +53,8 @@ msg_thread (void)
   int sandbox = 0;
   int i = 0, j = 0;
   u8 index = 0;
+  int count = 0;
+  uint64 tsc_tmp = 0;
 
   sandbox = get_pcpu_id ();
 
@@ -71,11 +78,11 @@ msg_thread (void)
       print ("\n");
       index++;
       /* 2 second interval */
-      sched_usleep (2000000);
+      sched_usleep (SND_RATE);
     }
   } else {
     /* This is the receiver's code */
-    DLOG ("Msg Sender: Hello from 0x%x", msgt_id);
+    DLOG ("Msg Receiver: Hello from 0x%x", msgt_id);
     DLOG ("Message Channel for Sandbox %d is 0x%X", sandbox, phys_channels[sandbox]);
     channel = map_virtual_page (phys_channels[sandbox] | 3);
     mailbox = (u8*) channel;
@@ -84,7 +91,7 @@ msg_thread (void)
     for (;;) {
       while (index == mailbox[MB_SIZE - 1]) {
         //DLOG ("Sandbox %d: No messages in mailbox. Index is %d...", sandbox, index);
-        sched_usleep (10000);
+        sched_usleep (recv_rate[sandbox]);
       }
       /* Got new message */
       index = mailbox[MB_SIZE - 1];
@@ -92,11 +99,113 @@ msg_thread (void)
         print ("Channel Corrupted!\n");
         continue;
       }
+      if (count < 50) {
+        RDTSC (tsc_tmp);
+        if (count == 0)
+          logs[sandbox][count] = tsc_tmp;
+        else
+          logs[sandbox][count] = tsc_tmp - logs[sandbox][0];
+        count++;
+      } else {
+        logs[sandbox][0] = 0;
+        for (count = 0; count < 50; count++)
+          com1_printf ("Sandbox %d.%d: %llX\n", sandbox, count,
+                       logs[sandbox][count]);
+        com1_printf ("Sandbox %d: Msgt done!\n", sandbox);
+        exit_kernel_thread ();
+      }
       DLOG ("Sandbox %d got message from Sandbox %d: %d", sandbox, MSG_HOST, index);
-      print ("Got message: ");
-      putx (index);
-      print ("\n");
+      //print ("Got message: ");
+      //putx (index);
+      //print ("\n");
     }
+  }
+}
+
+#define RECV_SB           1
+#define META_PAGE         3
+#define IPC_SB_SIZE       1024
+#define NUM_ITERATIONS    5000
+
+static void
+ipc_send_thread (void)
+{
+  uint64 start = 0, end = 0;
+  int i = 0, j = 0, k = 0, m = 0;
+  uint32 index = 0;
+  void * channel = NULL;
+  void * data = NULL;
+  uint32 * meta = NULL;
+  volatile uint32 * mailbox = NULL;
+  int cpu;
+  int count = 1;
+
+  cpu = get_pcpu_id ();
+  logger_printf ("Sandbox %d start sending...\n", cpu);
+
+  channel = map_virtual_page (phys_channels[RECV_SB] | 3);
+  data = map_virtual_page (phys_channels[META_PAGE] | 3);
+  mailbox = (uint32*) channel;
+  meta = (uint32*) data;
+  meta[0] = 16; /* Start from 16 * 4 = 64 bytes */
+
+  sched_usleep (2000000);
+
+  unlock_kernel ();
+  sti ();
+  for (i = 1; i < 15; i++) {
+    RDTSC (start);
+    for (m = 0; m < NUM_ITERATIONS * ((count == 1) ? 100 : 1); m++) {
+      for (j = 0; j < count; j++) {
+        index++;
+        for (k = 0; k < meta[0] && k < IPC_SB_SIZE; k++) {
+          mailbox[k] = index;
+        }
+        meta[1] = index;
+        while (index == meta[1]);
+        index = meta[1];
+      }
+    }
+    RDTSC (end);
+    logger_printf ("Cycles: 0x%llX      Message Size: %d Count: %d\n",
+                   end - start, 4 * meta[0], count);
+    meta[0] = meta[0] * 2;
+    count = ((meta[0] >> 10) == 0) ? 1 : (meta[0] >> 10);
+  }
+
+  logger_printf ("Sender Done!\n");
+  exit_kernel_thread ();
+}
+
+static void
+ipc_recv_thread (void)
+{
+  void * channel = NULL;
+  volatile uint32 * mailbox = NULL;
+  void * data = NULL;
+  uint32 * meta = NULL;
+  int k = 0;
+  uint32 index = 0;
+  int cpu;
+
+  cpu = get_pcpu_id ();
+  logger_printf ("Sandbox %d start receiving...\n", cpu);
+  channel = map_virtual_page (phys_channels[RECV_SB] | 3);
+  data = map_virtual_page (phys_channels[META_PAGE] | 3);
+  mailbox = (uint32*) channel;
+  meta = (uint32*) data;
+
+  unlock_kernel ();
+  sti ();
+  for (;;) {
+    while (index == meta[1]);
+    index = meta[1] + 1;
+
+    for (k = 0; k < meta[0] && k < IPC_SB_SIZE; k++) {
+      mailbox[k] = index;
+    }
+    meta[1] = index;
+    //asm volatile ("wbinvd");
   }
 }
 
@@ -104,18 +213,7 @@ extern bool
 msgt_init (void)
 {
   int sandbox = 0;
-  int i;
   sandbox = get_pcpu_id ();
-
-  if (sandbox == 0) {
-    for (i = 0; i < 4; i++) {
-      phys_channels[i] = shm_alloc_phys_frame ();
-      virt_channels[i] = map_virtual_page (phys_channels[i] | 3);
-      memset ((void*) virt_channels[i], 0, 4096);
-      unmap_virtual_page (virt_channels[i]);
-      DLOG ("Message Channel for Sandbox %d with Physical Address: 0x%X", i, phys_channels[i]);
-    }
-  }
 
   msgt_id = start_kernel_thread ((u32) msg_thread, (u32) &msgt_stack[1023]);
   lookup_TSS (msgt_id)->cpu = 1;
@@ -124,12 +222,56 @@ msgt_init (void)
   return TRUE;
 }
 
+extern bool
+ipc_send_init (void)
+{
+  int sandbox = 0;
+  sandbox = get_pcpu_id ();
+  task_id ipc_id = 0;
+
+  ipc_id = start_kernel_thread ((u32) ipc_send_thread, (u32) &ipc_stack[1023]);
+  lookup_TSS (ipc_id)->cpu = 1;
+
+  DLOG ("Sandbox %d: Sender 0x%x created", sandbox, ipc_id);
+
+  return TRUE;
+}
+
+extern bool
+ipc_recv_init (void)
+{
+  int sandbox = 0;
+  sandbox = get_pcpu_id ();
+  task_id ipc_id = 0;
+
+  ipc_id = start_kernel_thread ((u32) ipc_recv_thread, (u32) &ipc_stack[1023]);
+  lookup_TSS (ipc_id)->cpu = 1;
+
+  DLOG ("Sandbox %d: Receiver 0x%x created", sandbox, ipc_id);
+
+  return TRUE;
+}
+
+extern bool
+msgt_mem_init (void)
+{
+  int i;
+
+  for (i = 0; i < 4; i++) {
+    phys_channels[i] = shm_alloc_phys_frame ();
+    virt_channels[i] = map_virtual_page (phys_channels[i] | 3);
+    memset ((void*) virt_channels[i], 0, 4096);
+    unmap_virtual_page (virt_channels[i]);
+    DLOG ("Message Channel for Sandbox %d with Physical Address: 0x%X", i, phys_channels[i]);
+  }
+
+  return TRUE;
+}
+
 #include "module/header.h"
 
 static const struct module_ops mod_ops = {
   .init = msgt_init
 };
-
-//DEF_MODULE (msgt, "Message Passing Test", &mod_ops, {});
 
 /* vi: set et sw=2 sts=2: */
