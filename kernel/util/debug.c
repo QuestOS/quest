85ee67dfa4d8a1e2a03bba99e2ff5ac5fe03f5cf
Richard West <richwest@timelord.bu.edu>
2012-02-10 19:27:02 -0500

    Fixed linker script and some data structures for new version of gcc.

--- kernel/util/debug.c
+++ kernel/util/debug.c
@@ -114,12 +114,12 @@ union v8 {
     u8 cd:4;
   };
   struct {
-    u8 a:2;
+    u8 _a_:2;
     u8 bcd:6;
   };
   struct {
     u8 abc:6;
-    u8 d:2;
+    u8 _d_:2;
   };
 } __attribute__((packed));
 
@@ -161,8 +161,8 @@ dump_page (u8 *addr)
 
 static int getc (void)
 {
-  while (!(inb (PORT1 + 5) & 1));
-  return inb (PORT1);
+  while (!(inb (serial_port1 + 5) & 1));
+  return inb (serial_port1);
 }
 
 void
