c0d01cdb8a06f8af85bd3ff50b2d25139d4e2222
Ye Li <liye@cs.bu.edu>
2014-10-28 13:42:32 -0400

    Updated Pololu service.

--- tests/pololu.c
+++ tests/pololu.c
@@ -39,9 +39,9 @@ typedef struct pololu_command {
   uint8_t data2;
 } pololu_command_t;
 
-pololu_command_t pcmd;
+pololu_command_t pcmd1, pcmd2;
 
-uint32_t prev_index = 0;
+uint32_t prev_index1 = 0, prev_index2 = 0;
 
 int
 pololu_set_abspos (uint32_t ssc, uint8_t servo, uint32_t pos)
@@ -64,28 +64,44 @@ pololu_enable (uint32_t ssc, uint8_t servo)
 int
 main ()
 {
+  int res = 0;
   uint sandbox = socket_get_sb_id();
-  vshm_async_channel_t async_channel;
+  vshm_async_channel_t async_channel1, async_channel2;
   printf("In %s sandbox %u\n", __FILE__, sandbox);
   if (sandbox == 0) {
-    int res = mk_vshm_async_channel (&async_channel, 1234, sizeof(pololu_command_t),
-                                     0x1 << LINUX_SANDBOX,
-                                     VSHM_CREATE | VSHM_ALL_ACCESS);
+    res = mk_vshm_async_channel (&async_channel1, 1234, sizeof(pololu_command_t),
+                                 0x1 << LINUX_SANDBOX,
+                                 VSHM_CREATE | VSHM_ALL_ACCESS);
     printf("%d: res = %d\n", sandbox, res);
-    pcmd.index = 0;
+    res = mk_vshm_async_channel (&async_channel2, 4321, sizeof(pololu_command_t),
+                                 0x1 << LINUX_SANDBOX,
+                                 VSHM_CREATE | VSHM_ALL_ACCESS);
+    printf("%d: res = %d\n", sandbox, res);
+    pcmd1.index = pcmd2.index = 0;
     pololu_enable (0, 0);
     pololu_set_abspos (0, 0, 1500);
     usleep(3000000);
     pololu_set_abspos (0, 0, 4500);
     printf ("Waiting for new commands...\n");
     while(1) {
-      vshm_async_channel_read (&async_channel, &pcmd);
-      if (pcmd.index != prev_index) {
-        printf ("Got new command (%d,%d,%d,%d,%d)!\n", pcmd.ssc, pcmd.servo, pcmd.cmd,
-                pcmd.data1, pcmd.data2);
-        syscall_pololu_send_cmd (pcmd.ssc, pcmd.servo, pcmd.cmd, pcmd.data1, pcmd.data2);
-        prev_index = pcmd.index;
-        printf ("Command index=%d\n", prev_index);
+      /* Process channel 1 */
+      vshm_async_channel_read (&async_channel1, &pcmd1);
+      if (pcmd1.index != prev_index1) {
+        printf ("Got new command on channel1 (%d,%d,%d,%d,%d)!\n", pcmd1.ssc, pcmd1.servo, pcmd1.cmd,
+                pcmd1.data1, pcmd1.data2);
+        syscall_pololu_send_cmd (pcmd1.ssc, pcmd1.servo, pcmd1.cmd, pcmd1.data1, pcmd1.data2);
+        prev_index1 = pcmd1.index;
+        printf ("Command index=%d\n", prev_index1);
+      }
+
+      /* Process channel 2 */
+      vshm_async_channel_read (&async_channel2, &pcmd2);
+      if (pcmd2.index != prev_index2) {
+        printf ("Got new command on channel2 (%d,%d,%d,%d,%d)!\n", pcmd2.ssc, pcmd2.servo, pcmd2.cmd,
+                pcmd2.data1, pcmd2.data2);
+        syscall_pololu_send_cmd (pcmd2.ssc, pcmd2.servo, pcmd2.cmd, pcmd2.data1, pcmd2.data2);
+        prev_index2 = pcmd2.index;
+        printf ("Command index=%d\n", prev_index2);
       }
     }
     exit (EXIT_SUCCESS);
