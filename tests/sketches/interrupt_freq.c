5701dffc5c17df6401e2ce2db1a7284b6090a26f
Tom Cheng <czq@bu.edu>
2014-12-31 14:50:32 -0500

    added another two experiments

---
+++ tests/sketches/interrupt_freq.c
@@ -0,0 +1,36 @@
+#include <pin.h>
+#include <ardutime.h>
+#include <arduutils.h>
+
+volatile int counter;
+int status = LOW;
+int FLIP_N = 4000;
+unsigned long start_t, end_t;
+
+void
+IntHandler()
+{
+  counter++;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+	unsigned long long start_t, end_t;
+  attachInterrupt(2, IntHandler, CHANGE);
+  pinMode(13, OUTPUT);
+  int i;
+
+	printf("starting...\n");
+	rdtsc(&start_t);
+  for (i = 1; i <= FLIP_N; i++) {
+    digitalWrite(13, status = !status);
+    delay(100);
+  }
+	rdtsc(&end_t);
+	printf("counter is %d\n", counter);
+	print_long_long_hex(end_t - start_t);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+}
---
+++ tests/sketches/prime_single.c
@@ -0,0 +1,49 @@
+#include <ardutime.h>
+#include <arduutils.h>
+
+unsigned long long
+find_prime (int NUM_NB)    
+{    
+  int i, j, count = 0;
+  unsigned long long start = 0, end = 0;
+  unsigned int number = 0;
+
+  printf ("Find prime in 1 to %d\n", NUM_NB);
+
+  rdtsc(&start);
+  for (i = 0; i < NUM_NB; i++) {
+    number++;
+    if (number <= 3) {
+      if ((number == 2) || (number == 3)) {
+        count++;
+        continue;
+      } else {
+        continue;
+      }
+    }
+    if ((number % 2) == 0) continue;
+    for (j = 3; j < number; j += 2) {
+      if ((number % j) == 0) break;
+    }
+    if (j >= number) count++;
+  }
+  rdtsc(&end);
+
+  printf ("Counting finished! %d prime numbers found.\n", count);
+  return (end - start);
+}
+
+loop() {
+	unsigned long long res;
+	int N = 80000;
+
+	res = find_prime(N);
+  printf("C=20, Cycles: ");
+	print_long_long_hex(res);
+	printf("\n");
+	while (1);
+}
+
+setup()
+{
+}
