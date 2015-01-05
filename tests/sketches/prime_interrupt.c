dc1ec2da519b6c479b141ed0e87444ef3f79132c
Tom Cheng <czq@bu.edu>
2015-01-04 23:46:52 -0500

    added a new experiment

---
+++ tests/sketches/prime_interrupt.c
@@ -0,0 +1,63 @@
+#include <ardutime.h>
+#include <arduutils.h>
+#include <pin.h>
+
+int counter;
+
+unsigned long long
+find_prime (int NUM_NB)    
+{    
+  int i, j, count = 0;
+  unsigned long long start = 0, end = 0;
+  unsigned int number = 0;
+
+  printf ("Find prime in 1 to %d\n", NUM_NB);
+
+  rdtsc(&start);
+	//print_long_long_hex(start);
+  for (i = 0; i < NUM_NB; i++) {
+    number++;
+    if (number <= 3) {
+      if ((number == 2) || (number == 3)) {
+        count++;
+        continue;
+      } else {
+        continue;
+      }
+    }
+    if ((number % 2) == 0) continue;
+    for (j = 3; j < number; j += 2) {
+      if ((number % j) == 0) break;
+    }
+    if (j >= number) count++;
+  }
+  rdtsc(&end);
+	//print_long_long_hex(end);
+
+  printf ("Counting finished! %d prime numbers found.\n", count);
+  return (end - start);
+}
+
+void
+IntHandler()
+{
+	counter++;
+}
+
+loop()
+{
+	unsigned long long res;
+	int N = 80000;
+
+	printf("starting...\n");
+	res = find_prime(N);
+  printf("C=80, Cycles: ");
+	print_long_long_hex(res);
+	printf("counter is %d\n", counter);
+	while (1);
+}
+
+setup()
+{
+  attachInterrupt(2, IntHandler, CHANGE);
+}
